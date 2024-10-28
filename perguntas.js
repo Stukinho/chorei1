8e4a09856cdd979a8d6247c31b695e9b2b5f9407
Lucão 22 cm <142432774+LucasServare1@users.noreply.github.com>
2024-10-28 09:35:22 -0300

    fjh

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'perguntas gerais',
+    ' Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?',
+    'Tem entre 4 a 6 litros. São retirados 450 mililitros'
+)
+
+criaCartao(
+'pergunta gerais',
+'De quem é a famosa frase “Penso, logo existo”?',
+'Descartes'
+
+criaCartao(
+    'perguntas gerais',
+    'De onde é a invenção do chuveiro elétrico?',
+    'brasil',
+)
+)
