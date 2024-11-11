559ae1299534d937eb7e8894545fe4a7bf7ddad6
Stukinho <142433899+Stukinho@users.noreply.github.com>
2024-11-11 09:35:52 -0300

    grt44

--- perguntas.js
+++ perguntas.js
@@ -29,3 +29,28 @@ criaCartao(
     ' Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
     'João Goulart'
 )
+
+criaCartao(
+    `pergunta gerais`
+    'Quem foi a primeira pessoa a viajar no Espaço?'
+    'Yuri Gagarin.'
+)
+
+
+criaCartao(
+    'perguntas gerais'
+    'Qual a montanha mais alta do mundo?'
+    'Monte Everest'
+)
+
+criaCartao(
+    'perguntas gerais'
+    'Onde se localiza Machu Picchu?'
+    'Peru'
+)
+
+criaCartao(
+    'perguntas gerais'
+    'Que país tem o formato de uma bota?'
+    'italia'
+)
